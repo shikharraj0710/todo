7d6905f88b06431bf25dacbb618a0b8036ba31e2
Shikhar Raj Mishra <shikharraj921@gmail.com>
2022-06-13 12:02:20 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -136,6 +136,11 @@ app.get("/:url", function(req, res) {
     })
 })
 
-app.listen(3000, function() {
-    console.log("Server Started at 3000!!!")
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function() {
+    console.log("Server Started at " + port);
 });
